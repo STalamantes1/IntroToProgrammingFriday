f1ae9be5b70c1ccae0034975b345d4ec07dc7835
STalamantes1 <STalamantes1@csi.edu>
2026-09-11 09:46:24 -0600

    added examples

--- Project1/debug-1 (1).py
+++ Project1/debug-1 (1).py
@@ -3,11 +3,22 @@
 
 # This program should add three numbers together.
 
-total = 0
-num1 = input("What's the first number? >")
-total = num1
-num2 = input("What's the second number? >")
-total = num2
-num3 = input("What's the third number? >")
-total = num3
-print(f"Total is: {total}")
+# total = 0
+# num1 = int(input("What's the first number? >"))
+# total = num1
+# num2 = input("What's the second number? >")
+# total = num2
+# num3 = input("What's the third number? >")
+# total = num3
+# print(f"Total is: {total}")
+
+print("This converts C to F ")
+c=int(input("Enter your temp in celcius "))
+f=(c*9/5)+32
+print(f"Your temp of {c} converted to f is {f}")
+
+print("This program tells your age")
+current_year=int(input("What year is it?"))
+birth_year=int(input("Enter the year you were born"))
+age=current_year-birth_year
+print(f"You are {age} years old! ")
